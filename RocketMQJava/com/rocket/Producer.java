acc72af23f2232f8cfdc7f26847c9224f7cf5e14
jayasimha <jayasimhajay1994@gmail.com>
2018-04-02 20:17:26 +0530

    RocketMQ and Serive fabric stateful

---
+++ RocketMQJava/com/rocket/Producer.java
@@ -0,0 +1,29 @@
+package com.rocket;
+
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+public class Producer {
+    public static void main(String[] args) throws MQClientException, InterruptedException {
+        DefaultMQProducer producer = new DefaultMQProducer("please_rename_unique_group_name");
+
+        producer.start();
+
+        for (int i = 0; i < 10; i++) {
+            try {
+                Message msg = new Message("TopicTest","TagA",("Hello " + i).getBytes(RemotingHelper.DEFAULT_CHARSET));
+
+                SendResult sendResult = producer.send(msg);
+
+                System.out.printf("%s%n", sendResult);
+            } catch (Exception e) {
+                e.printStackTrace();
+                Thread.sleep(1000);
+            }
+        }
+        producer.shutdown();
+    }
+}
